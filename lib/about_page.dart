b57fe32387b08b0c48701aa32af7e84caada0ccc
waynedsz <waynedesouza33@gmail.com>
2025-11-29 20:56:24 +0000

    Update about page to display text

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -5,9 +5,27 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("About the Union Shop"),
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Center(
+              child: Text(
+                "About Page",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Welcome to the Union Shop! We are on a mission to give you the best University branded products",
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
