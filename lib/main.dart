15b2850c993960130c39e27f04a6803e6de55ec7
waynedsz <waynedesouza33@gmail.com>
2025-11-29 19:59:49 +0000

    made browse products button on home page nav to product page

--- lib/main.dart
+++ lib/main.dart
@@ -213,7 +213,9 @@ class HomeScreen extends StatelessWidget {
                         ),
                         const SizedBox(height: 32),
                         ElevatedButton(
-                          onPressed: placeholderCallbackForButtons,
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/product');
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF4d2963),
                             foregroundColor: Colors.white,
