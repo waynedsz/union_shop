6797ce6be90bf7bb8407a376687df6f772ee65ed
waynedsz <waynedesouza33@gmail.com>
2025-12-02 18:47:27 +0000

    created spacing under the appbar

--- lib/pages/collections_page.dart
+++ lib/pages/collections_page.dart
@@ -29,6 +29,7 @@ class _CollectionsPageState extends State<CollectionsPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              const SizedBox(height: 16),
               const SizedBox(height: 8),
               const Text(
                 "Collections",
