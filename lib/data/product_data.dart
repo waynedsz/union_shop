2988c68450701d502794133cddd0da3769d8edfc
waynedsz <waynedesouza33@gmail.com>
2025-12-04 13:23:48 +0000

    Add new product_data.dart to allow for mapping of names and files

---
+++ lib/data/product_data.dart
@@ -0,0 +1,42 @@
+final Map<String, List<Map<String, String>>> collectionProducts = {
+  'Hoodies': [
+    {
+      'name': 'Purple Hoodie',
+      'image': 'assets/images/products/hoodie_1.png',
+    },
+    {
+      'name': 'White Hoodie',
+      'image': 'assets/images/products/hoodie_2.png',
+    },
+  ],
+  'T-Shirts': [
+    {
+      'name': 'Beige Crest T-Shirt',
+      'image': 'assets/images/products/tshirt_1.png',
+    },
+    {
+      'name': 'Grey Crest T-Shirt',
+      'image': 'assets/images/products/tshirt_2.png',
+    },
+  ],
+  'Accessories': [
+    {
+      'name': 'Tote Bag',
+      'image': 'assets/images/products/accessory_tote.png',
+    },
+    {
+      'name': 'Beanie',
+      'image': 'assets/images/products/accessory_beanie.png',
+    },
+  ],
+  'New Arrivals': [
+    {
+      'name': 'Notebook',
+      'image': 'assets/images/products/notebook.png',
+    },
+    {
+      'name': 'Water Bottle',
+      'image': 'assets/images/products/water_botlle.png',
+    },
+  ],
+};
