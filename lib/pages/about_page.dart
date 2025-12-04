0ec8c815cc9fcc89c69232157af1cd3b517d951a
waynedsz <waynedesouza33@gmail.com>
2025-12-04 13:35:51 +0000

    added the footer widget

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/footer.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -10,26 +11,34 @@ class AboutPage extends StatelessWidget {
         title: const Text('About Us'),
         automaticallyImplyLeading: true,
       ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Center(
-              child: Text(
-                "About Page",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: const Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 50),
+                  Center(
+                    child: Text(
+                      "About Page",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      "Welcome to the Union Shop! We are on a mission to give you the best University branded products",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                "Welcome to the Union Shop! We are on a mission to give you the best University branded products",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
-        ),
+          ),
+          Footer(),
+        ],
       ),
     );
   }
