86c5bcdc40cd070756ad894ec80356cb765c7d51
waynedsz <waynedesouza33@gmail.com>
2025-12-04 17:36:47 +0000

    Create new CartState to manage the cart

---
+++ lib/reusable_content/cart_state.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:union_shop/reusable_content/cart_item.dart';
+import 'package:union_shop/reusable_content/product.dart';
+
+class CartState extends ChangeNotifier {
+  final List<CartItem> items = [];
+
+  void addToCart(Product product) {
+    final index = items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      items[index] = CartItem(
+        product: items[index].product,
+        quantity: items[index].quantity + 1,
+      );
+    } else {
+      items.add(CartItem(product: product, quantity: 1));
+    }
+    notifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    final index = items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      final existing = items[index];
+      if (existing.quantity > 1) {
+        items[index] = CartItem(
+          product: existing.product,
+          quantity: existing.quantity - 1,
+        );
+      } else {
+        items.removeAt(index);
+      }
+      notifyListeners();
+    }
+  }
+
+  void clearCart() {
+    items.clear();
+    notifyListeners();
+  }
+}
