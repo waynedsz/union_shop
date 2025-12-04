6f8f091bae3e4b4294aff6f2a20f9132797a4085
waynedsz <waynedesouza33@gmail.com>
2025-12-04 13:55:17 +0000

    Removed the hardcoded footer

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/footer.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -110,19 +111,7 @@ class ProductPage extends StatelessWidget {
             ),
           ),
           // Footer at the bottom
-          Container(
-            width: double.infinity,
-            color: Colors.grey[50],
-            padding: const EdgeInsets.all(24),
-            child: const Text(
-              'Placeholder Footer',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
+          const Footer(),
         ],
       ),
     );
