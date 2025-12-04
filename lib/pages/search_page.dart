d3dedd766c47555d1d9bc1033f0f61dc5e80963c
waynedsz <waynedesouza33@gmail.com>
2025-12-04 21:28:04 +0000

    Convert SearchPage to StatefulWidget and add TextEditingController

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,18 +22,19 @@ class SearchPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Search'),
       ),
-      body: const Column(
+      body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
             child: TextField(
-              decoration: InputDecoration(
+              controller: _searchController,
+              decoration: const InputDecoration(
                 labelText: 'Search',
                 border: OutlineInputBorder(),
               ),
             ),
           ),
-          Expanded(
+          const Expanded(
             child: Center(
               child: Text('Start typing to search'),
             ),
