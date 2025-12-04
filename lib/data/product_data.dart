31beb74012117fc41cf196a5463a7afe45c8abca
waynedsz <waynedesouza33@gmail.com>
2025-12-04 13:45:43 +0000

    Added pricing to product data

--- lib/data/product_data.dart
+++ lib/data/product_data.dart
@@ -3,40 +3,48 @@ final Map<String, List<Map<String, String>>> collectionProducts = {
     {
       'name': 'Purple Hoodie',
       'image': 'assets/images/products/hoodie_1.png',
+      'price': '£35.00',
     },
     {
       'name': 'White Hoodie',
       'image': 'assets/images/products/hoodie_2.png',
+      'price': '£32.00',
     },
   ],
   'T-Shirts': [
     {
       'name': 'Beige Crest T-Shirt',
       'image': 'assets/images/products/tshirt_1.png',
+      'price': '£18.00',
     },
     {
       'name': 'Grey Crest T-Shirt',
       'image': 'assets/images/products/tshirt_2.png',
+      'price': '£18.00',
     },
   ],
   'Accessories': [
     {
       'name': 'Tote Bag',
       'image': 'assets/images/products/accessory_tote.png',
+      'price': '£10.00',
     },
     {
       'name': 'Beanie',
       'image': 'assets/images/products/accessory_beanie.png',
+      'price': '£12.00',
     },
   ],
   'New Arrivals': [
     {
       'name': 'Notebook',
       'image': 'assets/images/products/notebook.png',
+      'price': '£6.00',
     },
     {
       'name': 'Water Bottle',
       'image': 'assets/images/products/water_bottle.png',
+      'price': '£14.00',
     },
   ],
 };
