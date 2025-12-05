29e833fa88db88f11efe0d3c96e4d82b90c0963c
waynedsz <waynedesouza33@gmail.com>
2025-12-05 02:37:06 +0000

    Added onPrintShackPressed callback to CollectionsPage header

--- lib/pages/collections_page.dart
+++ lib/pages/collections_page.dart
@@ -3,6 +3,7 @@ import 'package:union_shop/reusable_content/footer.dart';
 import 'package:union_shop/reusable_content/collection_widgets/collection_tile.dart';
 import 'package:union_shop/reusable_content/product_widgets/product_data.dart';
 import 'package:union_shop/reusable_content/header.dart';
+import 'package:union_shop/reusable_content/home_screen_widgets/navigation_controller.dart';
 
 class CollectionsPage extends StatelessWidget {
   const CollectionsPage({super.key});
@@ -29,6 +30,7 @@ class CollectionsPage extends StatelessWidget {
         onCartPressed: () {
           Navigator.pushNamed(context, '/cart');
         },
+        onPrintShackPressed: () => Navigator.pushNamed(context, '/print-shack'),
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
